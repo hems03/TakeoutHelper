d0c37a504011711e6e4073efb6f76a0e8b5183d6
Hemanth <hemuchiluka3@gmail.com>
2016-12-27 12:03:16 -0500

    added food item selection

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express=require('express');
+var request=require('request');
 var app=express();
 var path = require('path');
 var Bing = require('node-bing-api')({ accKey: "20aacff8d7144aa1b2e0befe613b89a7" });
@@ -14,8 +15,20 @@ var handlebars = require('express3-handlebars').create({
     }
 });
 
+request('https://rumobile.rutgers.edu/1/rutgers-dining.txt', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var menuObj=JSON.parse(body);
+    menuObj.forEach(function(hallObj){
+    	console.log(hallObj.meals[hallObj.meals.length-2]);
+    });
+    
 
-var foods=["tacos","burritos","chicken nuggets","pizza","chicken parmesan"];
+  }
+});
+
+
+
+var foods=["tacos","burritos","chicken_nuggets"];
 var foodObjs=[];
 
 function getFoodImages(ruFoodIndex, ruFoods){
@@ -31,14 +44,18 @@ function getFoodImages(ruFoodIndex, ruFoods){
 }
 getFoodImages(0,foods);
 
-var User=require('./models/user.js')
+var User=require('./models/user.js');
 var mongoose=require('mongoose');
 var opts={
 	server:{
-		socketOptions:{keepAlive:1}
+		socketOptions:{
+			keepAlive:30000,
+			 socketTimeoutMS: 30000,
+      		connectionTimeoutMS: 30000
+		}
 	}
-}
-mongoose.connect("mongodb://hems03:bobby007@ds143588.mlab.com:43588/takeout");
+};
+//mongoose.connect("mongodb://hems03:bobby007@ds143588.mlab.com:43588/takeout",opts);
 
 var Materialize = require('node-materialize');
 app.use(express.static(__dirname + "/public"));
@@ -60,10 +77,10 @@ app.post('/process',function(req,res){
 		last_name:req.body.last_name,
 		phone_number:req.body.phone_number
 	})
-	newUser.save(function(err,res){
+	/*newUser.save(function(err,res){
 		if(err)return console.error(err);
 		console.log("User Saved");
-	})
+	})*/
 	res.render('foods',{name:req.body.first_name,foods:foodObjs});
 })
 
